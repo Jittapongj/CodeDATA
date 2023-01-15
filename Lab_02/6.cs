a5956418c6930f89b28ce97ef4c1898970da2d3a
Jittapong Jongjai <118198788+Jittapongj@users.noreply.github.com>
2023-01-15 20:33:48 +0700

    Add files via upload

---
+++ Lab_02/6.cs
@@ -0,0 +1,14 @@
+using System;
+class Variable
+{
+    public static void Main (string[] args)
+    {
+        int year = 1990;
+        string name = "Thomas";
+        string nationality = "American";
+
+        Console.WriteLine ("His name is {0}", name);
+        Console.WriteLine ("He was born in {0}", year);
+        Console.WriteLine ("He is {0}", nationality);
+    }
+}
---
+++ Lab_02/9.js
@@ -0,0 +1,6 @@
+let site = "marcuscode";
+let fullname = "Matteo Marcus";
+let year = "2020";
+console.log("Length of site: " + site.length);
+console.log("Length of fullname: " + fullname.length);
+console.log("Length of year: " + year.length);
